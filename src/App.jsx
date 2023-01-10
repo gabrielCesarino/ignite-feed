1f79995bef87603895cc8d16de7dce13360c8cb2
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2023-01-10 17:29:59 -0300

    style: global

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { Header } from "./components/Header"
+import { Post } from "./Post"
+import './global.css'
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+    <Header />
+      <Post
+        author="Diego"
+        content="lorem10"
+      />
+      <Post
+        author="Teste"
+        content="Tes111"
+      />
+    </>
   )
 }
 
